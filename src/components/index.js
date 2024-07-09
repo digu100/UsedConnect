4cba349884e00e71c3d1519dbb19d1d72fadcc73
Diganta Dutta <duttadiganta100@gmail.com>
2024-07-09 21:28:24 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,60 @@
+/*import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Post from "../pages/Post";
+import PostForm from "./post-form/PostForm";
+import Postcard from "./PostCard";
+import AuthLayout from "./AuthLayout"
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Post,
+    PostForm,
+    Postcard,
+    AuthLayout,
+}*/
+
+import Select from "./Select";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./input";
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
+}
+
---
+++ src/pages/Login.jsx
@@ -0,0 +1,34 @@
+/*import React from 'react'
+
+//problem here
+import { Login as loginComponent } from '../components'
+function Login() {
+  return (
+    <div className='py-8'>
+        <loginComponent />
+    </div>
+  )
+}
+
+export default Login*/
+
+import React from 'react'
+
+//problem here
+import { Login } from '../components'
+function Login() {
+  return (
+    <div className='py-8'>
+        <Login/>
+    </div>
+  )
+}
+
+export default Login
+
+
+
+
+
+
+
